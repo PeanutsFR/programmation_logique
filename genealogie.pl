9aefec5224e1146af2ddb54af902d94d8ad38d39
Mehdi BOUAZIZ <baz.mhd@gmail.com>
2013-11-26 15:07:19 +0100

    Généalogie : terminé

--- genealogie.pl
+++ genealogie.pl
@@ -3,7 +3,7 @@
 %   +-------------------+
 
 
-        % a)
+        % --- a)
 
 /*
  * fils(pere, mere, fils).
@@ -24,33 +24,63 @@ fille(francois, sylvie, mathilde).
 fille(paul, virginie, stephanie).
 
 
-% --- Réponses ---
+        % --- b)
 
-/* Requete : jean et sophie ont-ils un(des) fils ?
-  -> ?- fils(jean, sophie, Fils). */
-
-/* Requete : de quels parents Nathalie est-elle la fille ?
-  -> ?- fille(Pere, Mere, nathalie). */
-
-/* Requete : déterminer la ou les filles de Sylvie.
-  -> ?- fille(Pere, sylvie, Fille). */
-
-% --------------------
+enfant(Pere, Mere, Enfant) :- fils(Pere, Mere, Enfant).
+enfant(Pere, Mere, Enfant) :- fille(Pere, Mere, Enfant).
 
 
-        % b)
+        % --- c)
+
+parent(Enfant, Parent) :- fils(Parent, _, Enfant).
+parent(Enfant, Parent) :- fils(_, Parent, Enfant).
+parent(Enfant, Parent) :- fille(Parent, _, Enfant).
+parent(Enfant, Parent) :- fille(_, Parent, Enfant).
 
 
+        % --- d)
+
+homme(X) :- fils(_, _, X).
+homme(X) :- fils(X, _, _).
+homme(X) :- fille(X, _, _).
+
+femme(X) :- fille(_, _, X).
+femme(X) :- fille(_, X, _).
+femme(X) :- fils(_, X, _).
 
 
+        % --- e)
+
+grand_parent(PetitEnfant, GrandParent)
+  :- parent(PetitEnfant, Parent), parent(Parent, GrandParent).
+
+grand_pere(PetitEnfant, GrandPere)
+  :- enfant(GrandPere, _, Fils), enfant(Fils, _, PetitEnfant).
+grand_pere(PetitEnfant, GrandPere)
+  :- enfant(GrandPere, _, Fille), enfant(_, Fille, PetitEnfant).
+
+grand_mere(PetitEnfant, GrandMere)
+  :- enfant(_, GrandMere, Fils), enfant(Fils, _, PetitEnfant).
+grand_mere(PetitEnfant, GrandMere)
+  :- enfant(_, GrandMere, Fille), enfant(_, Fille, PetitEnfant).
 
 
+        % --- f)
+
+frere(X, Frere) :- fils(Pere, Mere, Frere), fils(Pere, Mere, X).
+frere(X, Frere) :- fils(Pere, Mere, Frere), fille(Pere, Mere, X).
+
+/* Lorsqu on lance la requête frere(X, Frere), on obtient les couples :
+  _ (X, Frere)
+  _ (Frere, X).
+  Cela signifie qu on peut être son propre frère, ce qui n est pas normal. */
 
 
-/*
-:- initialization(main).
+        % --- g)
 
-main :- requete(X,Y), write(X), write(Y).
+ancetre(Descendant, Ancetre)
+  :- parent(Descendant, Ancetre).
 
-?- requete(X, Y).
-*/
+ancetre(Descendant, Ancetre)
+  :- parent(Descendant, Parent), ancetre(Parent, Ancetre).
+
